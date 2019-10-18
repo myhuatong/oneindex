d6004ca399ee223929dba659ed204db9ef644a86
绵阳华通留学 <56718816+myhuatong@users.noreply.github.com>
2019-10-18 17:36:03 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'quxKLSIP9:=lxheTO6609@=',
+  'client_id' => 'd9a57108-d8d1-4ecb-82b8-bfe45acd6835',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj77VxA84MA7oVLqD4eEGmaxmsmIZVbSbQVu6ToYTb6hG91YUd1pEXuPdPo6ZPAvleJzeHaXeap-RUzMbBTUFAS9-qvjtuV6Tgtzav5D7pg27BzBe_lpnzV3DTukXc65Hys9Q2AI42uGPbq7V3AV_51eqUHwOu08koKRkkfkaa_XLsUGSJ0mfWsywW5NkZ5gYh09_4naHP36PFqaFh5h98BI0sc90RiiiCtP4PJEV2z8IltzYS0RZwNr1enLyCQQ8v7Biuwil3hTABDFeq0G_nV5Og6YnbrK9Tw7fMjCUnBtTj2wRnj0jvpz757seMdPJ7OrfAcHFBz4RGpI5JuEKQgGgRA-sBuKzpa5Jgg7eVrptncwG2SSZX0CzXDmKKn5MZZ7_b6xhScWZdfey0e7gmV_tEQuXGsXfNEFI_7W44QMHpHaeDSiHuv_tr9ThgSBcrKj5OZnFnI_SH571vYOxVSiNqhhs9AkZOF_GJ3jnWfv8Qn2Uycavi4oM46qTPG8HfJR8oFhq4aj2bEowOoR17sU3qsurN4KxUMot9YBnKciIZOpHJYkXdKinquYaO0kcC3V9vh7DcIF5AcQmoc-RSbkp7uAtYrHXr7TcRtfK5SH_y-M38kkpMZMyQ1SS8Git9gUNSHoiVFj9KRdc21xKJHBRgSYkZ0qsb7pP8OqKOpbv5_er_RRZjDAYyhcEYZqzBn-soGvRBvSnMCfXbGVKU324lne3j6j8G29rUIi0FAtPmC6WiIoptfq0SyYa6_sPiJieviXHDWyvTv-BErwwmTs6VGTe_JfJ2oLofEO7qbqef_KtCiDUXagaKS0J2I-UHiIAA',
+);
